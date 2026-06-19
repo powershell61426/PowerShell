dde473bc766163d85eff6b854062d055eddc1d5d
peipei <peipeiyu00@gmail.com>
2026-06-19 15:45:40 -0400

    Create script for remote desktop wallpaper change.

---
+++ ChangeWallpaper.ps1
@@ -0,0 +1,8 @@
+﻿$s = New-PSSession -ComputerName PCName -Credential $cred
+Copy-Item "C:\LocalDestination\background.jpg" -Destination "C:\RemoteDestination\background.jpg" -ToSession $s
+Invoke-Command -Session $s -ScriptBlock{ 
+    Set-ItemProperty ` 
+    -Path "HKCU:\Control Panel\Desktop" ` 
+    -Name Wallpaper ` 
+    -Value "C:\LocalDestination\background.jpg" `
+}
