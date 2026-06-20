68f6ffced8f9ed40de220d1d2888439851334059
peipei <peipeiyu00@gmail.com>
2026-06-20 13:04:19 -0400

    Measure time to execute using a session.

---
+++ MeasureCommandSession.ps1
@@ -0,0 +1,7 @@
+﻿Measure-Command {
+    $s = New-PSSession -ComputerName PCName -Credential $cred
+    1..5 |
+    foreach {Invoke-Command $s {Get-Date}}
+    Remove-PSSession $s
+    } |
+    Format-Table -AutoSize TotalSeconds
